0a47e832aa8e37df31b960c4141fc4131a03128b
JyLiu <903803024@qq.com>
2024-07-24 16:47:25 +0800

    update: GlobalComponents.d.ts

--- components.d.ts
+++ components.d.ts
@@ -15,7 +15,6 @@ declare module 'vue' {
     ElDrawer: typeof import('element-plus/es')['ElDrawer']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElProgress: typeof import('element-plus/es')['ElProgress']
     ElRow: typeof import('element-plus/es')['ElRow']
